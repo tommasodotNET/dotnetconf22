6a77bcc743e38b82401366c8ac5d4359b58faeb4
Tommaso Stocchi <tommasostocchi@outlook.com>
2022-03-11 16:56:58 +0100

    Prefer using Dapr.Client

--- backend/city-service/city-api/Program.cs
+++ backend/city-service/city-api/Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDaprClient();
 var app = builder.Build();
 
-app.MapGet("/cities", async ([FromServices]DaprClient client) => 
+app.MapGet("/cities", async () => 
 {
     var cities = new []{"Rome", "Milan", "Venice"};
     var result = new List<CityWeatherModel>();
+    var daprClient = new DaprClientBuilder().Build();
 
     foreach(var city in cities)
     {
         // SAME AS: var weather = await httpClient.GetAsync($"http://localhost:{Environment.GetEnvironmentVariable("DAPR_HTTP_PORT")}/v1.0/invoke/"weather-service/method/weather/{city}");
-        var weather = await client.InvokeMethodAsync<List<WeatherForecast>>(HttpMethod.Get, "weather-service", $"weather/{city}");
+        var weather = await daprClient.InvokeMethodAsync<List<WeatherForecast>>(HttpMethod.Get, "weather-service", $"weather/{city}");
         
         result.Add(new CityWeatherModel
         {
