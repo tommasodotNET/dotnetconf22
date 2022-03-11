64273ab61fbd2de54b06de68f35ed2d98b5c4e1b
Tommaso Stocchi <tommasostocchi@outlook.com>
2022-03-11 16:49:35 +0100

    Weather API Implementation

--- backend/weather-service/weather-api/Program.cs
+++ backend/weather-service/weather-api/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/weather/{city}", (string city) => 
+{
+    var Summaries = new []{ "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"};
+    var rng = new Random();
+
+    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+    })
+    .ToArray();
+});
 
 app.Run();
+
+internal class WeatherForecast
+{
+    [JsonProperty("date")]
+    public DateTime Date { get; set; }
+
+    [JsonProperty("temperatureC")]
+    public int TemperatureC { get; set; }
+    
+    [JsonProperty("summary")]
+    public string Summary { get; set; }
+}
